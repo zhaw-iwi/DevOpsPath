01c1096f76bd67508e6cfe382e609099740332c9
Samuel <samuel.merki@celerato.ch>
2022-05-29 14:38:19 +0200

    Added build and test

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,22 +3,30 @@ pipeline {
 	stages {
 		stage('Checkout') { 
 			steps {
+				echo 'Checkout',
 				checkout([$class: 'GitSCM', branches: [[name: '*/09and10-CI']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/merkisam/DevOpsPath.git']]])
 			}
 		}
-		stage('Build') {
+		stage('Build Backend') {
 			steps {
+				echo 'Build Backend',
 				sh 'cd backend && gradle init'
 			}
 		}
-		stage('Test') { 
+		stage('Test Backend') { 
 			steps {
-				sh 'echo test'
+				echo 'Test Backend',
+				junit(
+					allowEmptyResults: true,
+					testResults: '**/test-results/test/*xml'
+				)
 			}
 		}
-		stage('Deploy') { 
+		stage('Build frontend') { 
 			steps {
-				sh 'echo deploy'
+				echo 'Build frontend',
+				sh 'npm install --prefix frontend',
+				sh 'npm run build --prefix frontend'
 			}
 		}
 	}
